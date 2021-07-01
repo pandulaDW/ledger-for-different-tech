63c84a563765f1ed74cf4c5091f88738e4295140
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2021-07-01 22:16:56 +0530

    Finished all tests

--- src/app/middlewares/ledgerRequestValidate.ts
+++ src/app/middlewares/ledgerRequestValidate.ts
@@ -1,4 +1,11 @@
 import { query } from "express-validator";
+import { Frequency } from "../models";
+
+// compiling regex expressions for improved performance
+const frequencyRegex = new RegExp(
+  `^(${Frequency.WEEKLY}|${Frequency.FORTNIGHTLY}|${Frequency.MONTHLY})$`
+);
+const timezoneRegex = new RegExp(/^[A-Z][a-z]+\/[A-Z][a-z]+$/);
 
 export const ledgerRequestValidation = [
   query("start_date")
@@ -15,7 +22,26 @@ export const ledgerRequestValidation = [
     .isISO8601()
     .withMessage("end_date is not a valid ISO date"),
 
-  query("frequency").notEmpty().withMessage("frequency cannot be undefined"),
-  query("weekly_rent").notEmpty().withMessage("weekly_rent cannot be undefined"),
-  query("timezone").notEmpty().withMessage("timezone cannot be undefined"),
+  query("frequency")
+    .notEmpty()
+    .withMessage("frequency cannot be undefined")
+    .bail()
+    .matches(frequencyRegex)
+    .withMessage(
+      `frequency should be one of ${Frequency.WEEKLY}, ${Frequency.FORTNIGHTLY} or ${Frequency.MONTHLY}`
+    ),
+
+  query("weekly_rent")
+    .notEmpty()
+    .withMessage("weekly_rent cannot be undefined")
+    .bail()
+    .isNumeric()
+    .withMessage("weekly_rent should be a numeric value"),
+
+  query("timezone")
+    .notEmpty()
+    .withMessage("timezone cannot be undefined")
+    .bail()
+    .matches(timezoneRegex)
+    .withMessage("timezone should be valid timezone name"),
 ];
--- src/test/ledgerRequest.test.ts
+++ src/test/ledgerRequest.test.ts
@@ -1,5 +1,6 @@
 import request from "supertest";
 import app from "../app/app";
+import { Frequency } from "../app/models";
 import { IndexType } from "../app/models/apiModels";
 
 const validRequest: IndexType = {
@@ -20,17 +21,25 @@ const convertToQueryString = (body: IndexType) => {
 };
 
 describe("ledger request integration tests", () => {
+  test("returns 200 status code when a valid request is used", async () => {
+    const response = await postRequest(convertToQueryString(validRequest));
+    expect(response.statusCode).toBe(200);
+  });
+
   test.each`
-    field            | value           | expectedMessage
-    ${"start_date"}  | ${undefined}    | ${"start_date cannot be undefined"}
-    ${"end_date"}    | ${undefined}    | ${"end_date cannot be undefined"}
-    ${"frequency"}   | ${undefined}    | ${"frequency cannot be undefined"}
-    ${"weekly_rent"} | ${undefined}    | ${"weekly_rent cannot be undefined"}
-    ${"timezone"}    | ${undefined}    | ${"timezone cannot be undefined"}
-    ${"start_date"}  | ${"test-date"}  | ${"start_date is not a valid ISO date"}
-    ${"start_date"}  | ${"2020-13-05"} | ${"start_date is not a valid ISO date"}
-    ${"end_date"}    | ${"test-date"}  | ${"end_date is not a valid ISO date"}
-    ${"end_date"}    | ${"2020-11-35"} | ${"end_date is not a valid ISO date"}
+    field            | value               | expectedMessage
+    ${"start_date"}  | ${undefined}        | ${"start_date cannot be undefined"}
+    ${"end_date"}    | ${undefined}        | ${"end_date cannot be undefined"}
+    ${"frequency"}   | ${undefined}        | ${"frequency cannot be undefined"}
+    ${"weekly_rent"} | ${undefined}        | ${"weekly_rent cannot be undefined"}
+    ${"timezone"}    | ${undefined}        | ${"timezone cannot be undefined"}
+    ${"start_date"}  | ${"test-date"}      | ${"start_date is not a valid ISO date"}
+    ${"start_date"}  | ${"2020-13-05"}     | ${"start_date is not a valid ISO date"}
+    ${"end_date"}    | ${"test-date"}      | ${"end_date is not a valid ISO date"}
+    ${"end_date"}    | ${"2020-11-35"}     | ${"end_date is not a valid ISO date"}
+    ${"frequency"}   | ${"test-frequency"} | ${"frequency should be one of WEEKLY, FORTNIGHTLY or MONTHLY"}
+    ${"weekly_rent"} | ${"test-rent"}      | ${"weekly_rent should be a numeric value"}
+    ${"timezone"}    | ${"America/"}       | ${"timezone should be valid timezone name"}
   `(
     "returns $expectedMessage when $field is $value",
     async ({ field, value, expectedMessage }) => {
@@ -45,4 +54,106 @@ describe("ledger request integration tests", () => {
       expect(response.body.validationErrors[field]).toBe(expectedMessage);
     }
   );
+
+  test("returns 200 status code when frequency is of desired value", () => {
+    Object.keys(Frequency).forEach(async (key) => {
+      const request = { ...validRequest };
+      request["frequency"] = key;
+      const response = await postRequest(convertToQueryString(request));
+      expect(response.statusCode).toBe(200);
+    });
+  });
+
+  test("returns correct response for a valid WEEKLY request", async () => {
+    const expected = {
+      ledgerLines: [
+        {
+          startDate: "2020-03-24T00:00:00.000Z",
+          endDate: "2020-03-30T00:00:00.000Z",
+          totalRent: 555,
+        },
+        {
+          startDate: "2020-03-31T00:00:00.000Z",
+          endDate: "2020-04-03T00:00:00.000Z",
+          totalRent: 317.14285714285717,
+        },
+      ],
+      numberOfLedgerLines: 2,
+    };
+
+    const request = {
+      ...validRequest,
+      end_date: "2020-04-03T00:00:00.000Z",
+      frequency: "WEEKLY",
+    };
+
+    const actual = await postRequest(convertToQueryString(request));
+    expect(expected).toEqual(actual.body);
+  });
+
+  test("returns correct response for a valid FORTNIGHTLY request", async () => {
+    const expected = {
+      ledgerLines: [
+        {
+          startDate: "2020-03-28T00:00:00.000Z",
+          endDate: "2020-04-10T00:00:00.000Z",
+          totalRent: 1110,
+        },
+        {
+          startDate: "2020-04-11T00:00:00.000Z",
+          endDate: "2020-04-24T00:00:00.000Z",
+          totalRent: 1110,
+        },
+        {
+          startDate: "2020-04-25T00:00:00.000Z",
+          endDate: "2020-05-03T00:00:00.000Z",
+          totalRent: 713.5714285714287,
+        },
+      ],
+      numberOfLedgerLines: 3,
+    };
+
+    const request = {
+      ...validRequest,
+      start_date: "2020-03-28T00:00:00.000Z",
+      end_date: "2020-05-03T00:00:00.000Z",
+      frequency: "FORTNIGHTLY",
+    };
+
+    const actual = await postRequest(convertToQueryString(request));
+    expect(expected).toEqual(actual.body);
+  });
+
+  test("returns correct response for a valid MONTHLY request", async () => {
+    const expected = {
+      ledgerLines: [
+        {
+          startDate: "2020-08-31T00:00:00.000Z",
+          endDate: "2020-09-30T00:00:00.000Z",
+          totalRent: 2411.607142857143,
+        },
+        {
+          startDate: "2020-10-01T00:00:00.000Z",
+          endDate: "2020-10-31T00:00:00.000Z",
+          totalRent: 2411.607142857143,
+        },
+        {
+          startDate: "2020-11-01T00:00:00.000Z",
+          endDate: "2020-11-27T00:00:00.000Z",
+          totalRent: 2140.714285714286,
+        },
+      ],
+      numberOfLedgerLines: 3,
+    };
+
+    const request = {
+      ...validRequest,
+      start_date: "2020-08-31T00:00:00.000Z",
+      end_date: "2020-11-27T00:00:00.000Z",
+      frequency: "MONTHLY",
+    };
+
+    const actual = await postRequest(convertToQueryString(request));
+    expect(expected).toEqual(actual.body);
+  });
 });
